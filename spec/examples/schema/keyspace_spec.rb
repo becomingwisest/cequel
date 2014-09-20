d130c6c61e9013e6b7061ee45d1d5c9a0e49c103
Christopher Evans <christopher.evans@reachlocal.com>
2014-09-20 16:49:46 -0700

    Forklift in spec/examples/spec_helper.rb

--- spec/examples/schema/keyspace_spec.rb
+++ spec/examples/schema/keyspace_spec.rb
@@ -1,5 +1,47 @@
 # -*- encoding : utf-8 -*-
-require_relative '../spec_helper'
+require File.expand_path('../../environment', __FILE__)
+require 'cequel'
+require 'tzinfo'
+
+Dir.glob(File.expand_path('../../support/**/*.rb', __FILE__)).each do |file|
+  require file
+end
+Dir.glob(File.expand_path('../../shared/**/*.rb', __FILE__)).each do |file|
+  require file
+end
+
+RSpec.configure do |config|
+  config.include(Cequel::SpecSupport::Helpers)
+  config.extend(Cequel::SpecSupport::Macros)
+
+  config.filter_run_excluding rails: ->(requirement) {
+    !Gem::Requirement.new(requirement).
+      satisfied_by?(Gem::Version.new(ActiveSupport::VERSION::STRING))
+  }
+=begin
+  unless defined? CassandraCQL
+    config.filter_run_excluding thrift: true
+  end
+
+  config.before(:all) do
+    cequel.schema.create!
+    Cequel::Record.connection = cequel
+    Time.zone = 'UTC'
+    I18n.enforce_available_locales = false
+    SafeYAML::OPTIONS[:default_mode] = :safe if defined? SafeYAML
+  end
+
+  config.after(:all) do
+    cequel.schema.drop!
+  end
+
+  config.after(:each) { Timecop.return }
+=end
+end
+
+if defined? byebug
+  Kernel.module_eval { alias_method :debugger, :byebug }
+end
 
 describe Cequel::Schema::Keyspace do
   describe "#create!" do
